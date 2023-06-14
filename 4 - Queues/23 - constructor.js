8245f701507697c3da7e768467c7ce62a3175e0c
caioagralemos <caioagralemos@live.com>
2023-06-14 18:57:58 -0300

    4 - Queues

---
+++ 4 - Queues/23 - constructor.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class Queue {
+	constructor(value) {
+        var newNode = new Node(value)
+        this.first = newNode
+        this.last = newNode
+        this.length = 1
+    }
+}
+ 
+
+ 
+let myQueue = new Queue(4);
